7bda6a19c1133c51d4d5a5e3383ed5d49774c900
Adaliz Membreno <adalizitachi11@gmail.com>
2024-07-22 16:08:14 -0600

    add redesing header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import HomeScreen from './screens/home'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
